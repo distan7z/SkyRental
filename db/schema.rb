ff5d7a7e8c15d1b2b5c4fadc284afe26287c5ef8
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-02-28 16:53:18 +0100

    new seed with dynamic description

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_145717) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ Drone.destroy_all
 User.destroy_all
 
 puts "DB cleaned"
+
 DRONE_BRANDS_MODELS = [
   {
     "brand": "DJI",
@@ -138,7 +139,33 @@ DRONE_BRANDS_MODELS = [
   }
 ]
 
-DRONE_DESCRIPTIONS = Faker::Lorem.paragraph
+DRONE_ANONYMOUS_DESCRIPTIONS = [
+  "A compact quadcopter with a built-in camera for aerial photography.",
+  "A foldable drone with a long battery life and obstacle avoidance sensors.",
+  "A powerful hexacopter with advanced GPS tracking and stabilization features.",
+  "A lightweight drone with a modular design and customizable payloads.",
+  "A rugged quadcopter with thermal imaging capabilities for search and rescue missions.",
+  "An agile drone with a 360-degree gimbal and real-time video transmission.",
+  "A high-speed racing drone with customizable LED lights and FPV goggles.",
+  "A waterproof quadcopter with a built-in air pump for underwater exploration.",
+  "A professional-grade drone with a three-axis gimbal and 4K camera.",
+  "A compact drone with gesture control and intelligent flight modes.",
+  "A versatile quadcopter with swappable arms and a modular camera system.",
+  "An industrial drone with a heavy payload capacity and long flight time.",
+  "A mini drone with optical flow sensors and one-button takeoff and landing.",
+  "A hybrid drone that can switch between fixed-wing and quadcopter modes.",
+  "A stabilized drone with an anti-jello camera mount for smooth aerial footage.",
+  "A drone with automatic obstacle avoidance and precision landing capabilities.",
+  "A long-range quadcopter with a high-definition camera and real-time transmission.",
+  "A lightweight drone with a 2-axis mechanical gimbal and active track mode.",
+  "A drone with a brushless motor and high-performance electronic speed controller.",
+  "An aerial robot with machine learning capabilities and advanced computer vision.",
+  "A quadcopter with a foldable design and 4K camera for cinematic shots.",
+  "A drone with a swiveling camera for capturing panoramic images and videos.",
+  "A drone with a GPS tracker and autonomous flight modes for easy navigation.",
+  "A drone with a high-strength carbon fiber frame and vibration dampening system.",
+  "A drone with a 360-degree obstacle avoidance system and gesture recognition."
+]
 
 
 
@@ -156,10 +183,11 @@ end
 
 def new_drones
   50.times do
+    i = rand(0..DRONE_BRANDS_MODELS.length-1)
     drone = Drone.new(
-      brand: DRONE_BRANDS_MODELS.sample[:brand],
-      model: DRONE_BRANDS_MODELS.sample[:models].sample,
-      description: DRONE_DESCRIPTIONS,
+      brand: DRONE_BRANDS_MODELS[i][:brand],
+      model: DRONE_BRANDS_MODELS[i][:model],
+      description: DRONE_ANONYMOUS_DESCRIPTIONS.sample,
       user: User.all.sample
     )
     drone.save!
